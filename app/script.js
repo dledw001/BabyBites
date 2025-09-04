db500d589ecb24d8bc5b1002908be7ac58d49a64
Dan Ledwith <dledw001@odu.edu>
2025-09-04 00:20:37 -0400

    small change

--- app/script.js
+++ app/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 	document.getElementById("call-api").onclick = async () => {
-		const response = await fetch("/api/hello?name=User");
+		const response = await fetch("/api/hello");
 		const data = await response.json();
 		document.getElementById("output").textContent = data.message;
 	};
