d528673e097ca8e95257b23889f06ee893be2510
Dan Ledwith <dledw001@odu.edu>
2025-09-14 15:48:08 -0400

    added CSRF_TRUSTED_ORIGINS

--- backend/settings.py
+++ backend/settings.py
@@ -33,7 +33,9 @@ SECRET_KEY = 'django-insecure-b=har_f&kq-n4$z#$rtpp!4#qbu%og$96n@pm!*n!iykg+fahm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "10.0.0.139", "*", ]
+ALLOWED_HOSTS = ["*", ]
+
+CSRF_TRUSTED_ORIGINS = ["https://babybites.dledwith.com", ]
 
 # Application definition
 
