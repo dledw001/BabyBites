d580492f2fc03aed9c71fe8bb0c351d7746017e7
Dan Ledwith <dledw001@odu.edu>
2025-09-14 01:27:59 -0400

    removed stale js

--- core/static/core/js/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-	document.getElementById("call-api").onclick = async () => {
-		const response = await fetch("/api/hello");
-		const data = await response.json();
-		document.getElementById("output").textContent = data.message;
-	};
-});
