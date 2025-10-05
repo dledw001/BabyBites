b561b690aefcb641d247a53556c6fe807d56d155
Martin Salisbury <msali005@odu.edu>
2025-10-05 13:42:43 -0400

    models and psql updates

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,18 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
+from .views import baby_list, baby_create, baby_edit, baby_delete
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('dashboard/', views.dashboard, name="dashboard"),
     path('register/', views.register, name="register"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-]
+    path('babies/', baby_list, name='baby-list'),
+    path('babies/new/', baby_create, name='baby-create'),
+    path('babies/<uuid:baby_id>/edit', baby_edit, name='baby-edit'),
+    path('babies/<uuid:baby_id>/delete/', baby_delete, name='baby-delete')
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
